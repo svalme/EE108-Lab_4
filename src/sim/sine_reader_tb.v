de032a92aa202d6bbd9f64f0eed95529075ff018
Maria Fernanda Velásquez <64380973+mfvr59@users.noreply.github.com>
2025-10-24 22:12:53 -0700

    final tb
    Added expected sample_ready checks for multiple test cases.

--- src/sim/sine_reader_tb.v
+++ src/sim/sine_reader_tb.v
@@ -24,20 +24,27 @@ module sine_reader_tb();
     end
 
     // Tests
+    reg exp_sample_ready;
     initial begin
 
         generate_next = 1'b0;
         step_size = 20'd1000;
 
-        #10; // wait one cycle
+        #30; // wait one cycle
 
         // Test #1
         generate_next = 1'b1;
         #10;  // 1 clock high
         generate_next = 1'b0;
         #10;  // 2-cycle wait for output
-
+        
+        exp_sample_ready = 1'b1;
         $display("[%0t] step_size=%0d sample_ready=%b sample=%0d", $time, step_size, sample_ready, sample);
+        $display("Expected sample_ready=%b, Actual sample_ready=%b", exp_sample_ready, sample_ready);
+        if (sample_ready == exp_sample_ready)
+            $display("PASS");
+        else
+            $display("FAIL");
 
         // Wait a few idle cycles before next pulse
         #20;
@@ -48,8 +55,14 @@ module sine_reader_tb();
         #10;
         generate_next = 1'b0;
         #10; // 2-cycle wait for output
-
+        
+        exp_sample_ready = 1'b1;
         $display("[%0t] step_size=%0d sample_ready=%b sample=%0d", $time, step_size, sample_ready, sample);
+        $display("Expected sample_ready=%b, Actual sample_ready=%b", exp_sample_ready, sample_ready);
+        if (sample_ready == exp_sample_ready)
+            $display("PASS");
+        else
+            $display("FAIL");
 
         #20; 
 
@@ -59,7 +72,14 @@ module sine_reader_tb();
         #10;
         generate_next = 1'b0;
         #10; // 2-cycle wait for output
+        
+        exp_sample_ready = 1'b1;
         $display("[%0t] step_size=%0d sample_ready=%b sample=%0d", $time, step_size, sample_ready, sample);
+        $display("Expected sample_ready=%b, Actual sample_ready=%b", exp_sample_ready, sample_ready);
+        if (sample_ready == exp_sample_ready)
+            $display("PASS");
+        else
+            $display("FAIL");
 
         #20;
 
@@ -69,7 +89,14 @@ module sine_reader_tb();
         #10;
         generate_next = 1'b0;
         #10;
+        
+        exp_sample_ready = 1'b1;
         $display("[%0t] step_size=%0d sample_ready=%b sample=%0d", $time, step_size, sample_ready, sample);
+        $display("Expected sample_ready=%b, Actual sample_ready=%b", exp_sample_ready, sample_ready);
+        if (sample_ready == exp_sample_ready)
+            $display("PASS");
+        else
+            $display("FAIL");
 
     end
 
